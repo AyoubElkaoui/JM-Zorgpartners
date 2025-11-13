203821bf20e13b01f3d9df60b74363e947594ee7
Ayoub Elkaoui <elkaoui.a@gmail.com>
2025-11-13 17:20:32 +0100

    fixed mobile nav

--- components/header.tsx
+++ components/header.tsx
@@ -10,6 +10,7 @@ import {
   MobileNavMenu,
 } from "@/components/ui/resizable-navbar";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 const navItems = [
@@ -27,7 +28,7 @@ export default function Header() {
     <Navbar className="!h-24">
       <NavBody className="!h-24">
         {/* Custom Logo */}
-        <a href="/" className="flex items-center mr-6 select-none">
+        <Link href="/" className="flex items-center mr-6 select-none">
           <Image
             src="/logo/logo-color.png"
             alt="JM Zorgpartners"
@@ -36,7 +37,7 @@ export default function Header() {
             className="h-20 w-auto"
             priority
           />
-        </a>
+        </Link>
         <NavItems items={navItems} />
         <div className="flex items-center gap-4">
           <NavbarButton
@@ -49,7 +50,7 @@ export default function Header() {
       </NavBody>
       <MobileNav>
         <MobileNavHeader>
-          <a href="/" className="flex items-center mr-6 select-none">
+          <Link href="/" className="flex items-center mr-6 select-none">
             <Image
               src="/logo/logo-color.png"
               alt="JM Zorgpartners"
@@ -58,7 +59,7 @@ export default function Header() {
               className="h-14 w-auto"
               priority
             />
-          </a>
+          </Link>
           <MobileNavToggle
             isOpen={isMobileMenuOpen}
             onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
@@ -69,22 +70,22 @@ export default function Header() {
           onClose={() => setIsMobileMenuOpen(false)}
         >
           {navItems.map((item, idx) => (
-            <a
+            <Link
               key={idx}
               href={item.link}
-              className="block w-full px-4 py-2 text-neutral-600"
+              className="block w-full px-4 py-2 text-black"
               onClick={() => setIsMobileMenuOpen(false)}
             >
               {item.name}
-            </a>
+            </Link>
           ))}
-          <a
+          <Link
             href="/contact"
             className="block w-full px-4 py-2 text-[#F37325] font-bold"
             onClick={() => setIsMobileMenuOpen(false)}
           >
             Contact
-          </a>
+          </Link>
         </MobileNavMenu>
       </MobileNav>
     </Navbar>
